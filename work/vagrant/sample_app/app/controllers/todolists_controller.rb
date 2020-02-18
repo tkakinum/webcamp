cf82a363a174d1ca931b12e2dd7d58d2234b1228
tkakinum <kt.g.retrieve@icloud.com>
2020-02-18 16:21:19 +0900

    [finish]3-6

--- work/vagrant/sample_app/app/controllers/todolists_controller.rb
+++ work/vagrant/sample_app/app/controllers/todolists_controller.rb
@@ -8,14 +8,18 @@ def create
 
 	list.save
 
-	redirect_to'/top'
+	redirect_to todolist_path(list.id)
 end
 
 def index
 	@lists = List.all
 end
 
-privates
+def show
+	@list = List.find(params[:id])
+end
+
+private
 
 def list_params
 	params.require(:list).permit(:title,:body)

834b52d2603a320281e60954bd28ba30da3fdace
tkakinum <kt.g.retrieve@icloud.com>
2020-02-18 18:35:04 +0900

    [finish]3-7

--- work/vagrant/sample_app/config/routes.rb
+++ work/vagrant/sample_app/config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'todolists/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top'=>'homes#top'
+	get 'todolists/new'
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get 'top'=>'homes#top'
 
-  post 'todolists' => 'todolists#create'
+	post 'todolists' => 'todolists#create'
 
-  get 'todolists' => 'todolists#index'
+	get 'todolists' => 'todolists#index'
 
-  get 'todolists/:id' => 'todolists#show',as: 'todolist'
+	get 'todolists/:id' => 'todolists#show',as: 'todolist'
 
-  get 'todolists/:id/edit' =>'todolist#edit',as: 'edit_todolist'
+	get 'todolists/:id/edit' =>'todolists#edit',as: 'edit_todolist'
+
+	patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 
 end

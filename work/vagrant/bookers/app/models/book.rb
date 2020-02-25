8ce60e4321af07d835264d08d16c7a957c5699ac
tkakinum <kt.g.retrieve@icloud.com>
2020-02-25 14:33:28 +0900

    4-8 make each page

--- work/vagrant/bookers/app/models/book.rb
+++ work/vagrant/bookers/app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
 
+validates :title,
+    presence: true
+
+validates :body,
+    presence: true
+
 end
--- work/vagrant/bookers/config/routes.rb
+++ work/vagrant/bookers/config/routes.rb
@@ -1,21 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'book#top'
 
   get 'top' => 'books#top'
-
-  post 'books' => 'books#create'
-
-  get 'books/:id' => 'books#show', as: 'book'
-
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-
-  patch 'books/:id' => 'books#update', as: 'update_book'
-
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-
+  resources :books
 end

bc3dafd2364c7addbbebd4cc911b070360e39f51
tkakinum <kt.g.retrieve@icloud.com>
2020-02-26 15:53:19 +0900

    [start]5-5

--- work/vagrant/meshiterro/config/routes.rb
+++ work/vagrant/meshiterro/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
   end

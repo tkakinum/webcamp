95e8b4131cb1fe6f081e5398d87f4112bb4119a4
tkakinum <kt.g.retrieve@icloud.com>
2020-02-26 12:43:43 +0900

    [finish]5-3

--- work/vagrant/meshiterro/config/initializers/application_controller_renderer.rb
+++ work/vagrant/meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '42ba0c16b40934b7fa00f6e3cb016c152c0856f278b8e04ecc72db015d6ee2a9eddb767eb13840d2af0435b775bb779ed8ac475a2e80f5ca7bf8602d73fe72a0'

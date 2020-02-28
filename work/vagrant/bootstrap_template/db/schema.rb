98b129942f02c8bbb47bd8ec0ba1ff5e583e1971
tkakinum <kt.g.retrieve@icloud.com>
2020-02-28 15:52:14 +0900

    [finish]5-8

--- work/vagrant/bootstrap_template/db/schema.rb
+++ work/vagrant/bootstrap_template/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004075558) do
+ActiveRecord::Schema.define(version: 2017_10_04_075558) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"

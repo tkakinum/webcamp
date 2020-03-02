9087a917688b2d72ba07971875afa01ab3132185
tkakinum <kt.g.retrieve@icloud.com>
2020-03-02 18:46:30 +0900

    5-9

--- work/vagrant/bookers2/app/models/post_image.rb
+++
@@ -1,8 +0,0 @@
-class PostImage < ApplicationRecord
-
-	belongs_to :user
-	attachment :image
-	validates :name, length: { in: 2..20 }
-	validates :name, presence: true
-	validates :introduction, presence: true
-end

d1a3b88b156fbb8217366444eeb28b9fd23f7b3e
tkakinum <kt.g.retrieve@icloud.com>
2020-03-03 15:04:26 +0900

    5-9

--- work/vagrant/bookers2/app/controllers/books_controller.rb
+++ work/vagrant/bookers2/app/controllers/books_controller.rb
@@ -1,4 +1,49 @@
 class BooksController < ApplicationController
 
+  def top
+    @book = Book.all
+  end
 
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @books =Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "can't be blank!"
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end

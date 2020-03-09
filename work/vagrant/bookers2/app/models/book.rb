873039ecc5deda0f6816f904ba9f714e5d63a283
tkakinum <kt.g.retrieve@icloud.com>
2020-03-09 15:05:13 +0900

    5-9

--- work/vagrant/bookers2/app/models/book.rb
+++ work/vagrant/bookers2/app/models/book.rb
@@ -1,8 +1,7 @@
 class Book < ApplicationRecord
 
-validates :title, presence: true
-validates :body, presence: true, length: { maximum: 200 }
-
-has_many :users, dependent: :destroy
-belongs_to :user
+	has_many :users, dependent: :destroy
+	belongs_to :user
+	validates :title, presence: true
+	validates :body, presence: true, length: { maximum: 200 }
 end

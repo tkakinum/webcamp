340e1720006d8e7d3fb727af39a138aa7e6eb5df
tkakinum <kt.g.retrieve@icloud.com>
2020-03-12 18:40:08 +0900

    Bookers2 repair

--- Java/app.js
+++ Java/app.js
@@ -1,4 +1,17 @@
-function addString(strA){
-    var addStr = "Hello " + strA;
-    return addStr;
+var　user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
+
+alert('あなたの選んだ手は' + user_hand + 'です。')
+
+function getJShand(){
+	var js_hand_num = Muth.floor( Math.random() * 3 );
+
+	if(js_hand_num == 0){
+		js_hand = "グー";
+	}else if(js_hand_num == 1){
+		js_hand = "チョキ";
+	}else if(js_hand_num == 2){
+		js_hand = "パー";
+	}
+
+	return js_hand;
 }
